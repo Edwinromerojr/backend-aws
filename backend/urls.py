3749e7970b3c52c4789d6fc81425be9b17c14223
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-16 23:10:24 +0800

    swagger

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Backend Online Shop API",
+        default_version="1.0.0",
+        description="This is swagger for our apis.",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('onlineshop.urls')),
+    path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema')
 ]
